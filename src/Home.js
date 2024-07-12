69668a4284f3f5fd6f56fa301950eb0230f7807c
Albean12 <130741408+Albean12@users.noreply.github.com>
2024-07-12 15:22:27 +0800

    REACT DOM FOR ONE SHOT PAGE

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <h1>Convenience Meets Technology</h1>;
+};
+
+export default Home;
---
+++ src/Units.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Units = () => {
+  return <h1>Our Units</h1>;
+};
+
+export default Units;
