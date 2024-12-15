6248b908325cf61aba30bbd1e8a97f1f55ab6bdf
G_Rodolfo <girondudut92@gmail.com>
2024-12-15 16:25:26 +0800

    update unit

--- src/Units.js
+++ src/Units.js
@@ -4,12 +4,18 @@ import './Units.css';
 const Units = () => {
   useEffect(() => {
     const rentals = [
-      { id: 1, title: "Male Bedspace for Rent in Quezon City at The Avenue Residences", location: "Talipapa, Quezon City", price: "3,500 monthly", availability: "2 Available", image: "unit1.jpg", verified: true, tag: "Dormy 474" },
-      { id: 2, title: "Room near UST", location: "Sampaloc, Manila", price: "4,000 monthly", availability: "1 Available", image: "unit2.jpg", verified: false, tag: "Dormy 123" },
-      { id: 3, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", verified: true, tag: "Dormy 567" },
-      { id: 4, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", verified: true, tag: "Dormy 567" },
-      { id: 5, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", verified: true, tag: "Dormy 567" },
-      
+      { id: 1, title: "Male Bedspace for Rent in Quezon City at The Avenue Residences", location: "Talipapa, Quezon City", price: "3,500 monthly", availability: "2 Available", image: "unit1.jpg", tag: "Dormy 474" },
+      { id: 2, title: "Room near UST", location: "Sampaloc, Manila", price: "4,000 monthly", availability: "1 Available", image: "unit2.jpg",  tag: "Dormy 123" },
+      { id: 3, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", tag: "Dormy 567" },
+      { id: 4, title: "Male Bedspace for Rent in Quezon City at The Avenue Residences", location: "Talipapa, Quezon City", price: "3,500 monthly", availability: "2 Available", image: "unit1.jpg", tag: "Dormy 474" },
+      { id: 5, title: "Room near UST", location: "Sampaloc, Manila", price: "4,000 monthly", availability: "1 Available", image: "unit2.jpg",  tag: "Dormy 123" },
+      { id: 6, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", tag: "Dormy 567" },
+      { id: 7, title: "Male Bedspace for Rent in Quezon City at The Avenue Residences", location: "Talipapa, Quezon City", price: "3,500 monthly", availability: "2 Available", image: "unit1.jpg", tag: "Dormy 474" },
+      { id: 8, title: "Room near UST", location: "Sampaloc, Manila", price: "4,000 monthly", availability: "1 Available", image: "unit2.jpg",  tag: "Dormy 123" },
+      { id: 9, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", tag: "Dormy 567" },
+      { id: 10, title: "Male Bedspace for Rent in Quezon City at The Avenue Residences", location: "Talipapa, Quezon City", price: "3,500 monthly", availability: "2 Available", image: "unit1.jpg", tag: "Dormy 474" },
+      { id: 11, title: "Room near UST", location: "Sampaloc, Manila", price: "4,000 monthly", availability: "1 Available", image: "unit2.jpg",  tag: "Dormy 123" },
+      { id: 12, title: "Condo Share w/ WiFi", location: "Taguig", price: "5,000 monthly", availability: "3 Available", image: "unit3.jpg", tag: "Dormy 567" },
     ];
 
     const renderRentals = () => {
