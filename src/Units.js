dbb4e5c66aa27e033ada2c73ba5a96a7f3e2886b
G_Rodolfo <girondudut92@gmail.com>
2024-12-15 16:01:56 +0800

    update unit.js

--- src/Units.js
+++ src/Units.js
@@ -1,6 +1,22 @@
-import React, { useState, useEffect } from "react";
-import "./Units.css";
+import React from 'react';
+import './Units.css';
+const Units = () => {
+  return (
+    <div className="Units">
+      <h1>Sampol</h1>
+      <p>For any inquiries or further information, feel free to reach us on our Facebook page.</p>
+      
+      {/* Facebook link */}
+      <a 
+        href="https://www.facebook.com/profile.php?id=61551676772866" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="facebook-link"
+      >
+        Visit Our Facebook Page
+      </a>
+    </div>
+  );
+};
 
-const UnitCard = ();
-
-export default Dormitory;
+export default Units;
